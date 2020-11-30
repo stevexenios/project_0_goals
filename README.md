0ac236a7bb4a83b173bdf1a57ff95ca243f835fe
xenios@uw.edu <27645504+stevexenios@users.noreply.github.com>
2020-11-29 16:48:03 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# project_0_goals
+Basic react app. Helps in understanding react:
+    * components
+    * props 
+    * state.
+
+## Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React, { useState } from 'react';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    {id: 'cg1', text:'Finish the Course'},
+    {id: 'cg2', text:'Learn all about the Course Main Topic'},
+    {id: 'cg3', text:'Help others in Q &amp; A'}
+  ]);
+
+  const addNewGoalHandler = (newGoal) => {
+    // setCourseGoals(courseGoals.concat(newGoal));
+    // setCourseGoals((previousCourseGoals) => {
+    //   return previousCourseGoals.concat(newGoal);
+    // });
+    setCourseGoals(previousCourseGoals => previousCourseGoals.concat(newGoal));
+  };
+
+  return <div className="course-goals">
+    <h2> Course Goals </h2>
+    <NewGoal onAddGoal={addNewGoalHandler}/>
+    <GoalList goals = {courseGoals}/>
+  </div>;
+}
+
+export default App;
